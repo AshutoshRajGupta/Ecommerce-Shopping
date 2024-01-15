9404ce130ceab60f7c1c07659dd6edd641fe3538
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-01-16 00:30:45 +0530

    backend created login register api created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 // configuring dotenv
 dotenv.config();
@@ -16,6 +17,10 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+//rest api
 app.get("/", (req, res) => {
   res.send({
     message: "welcome the ecommerce app",
