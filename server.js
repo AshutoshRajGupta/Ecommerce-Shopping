9bc376168f4183b034711f7f7089ec9b2f24c70d
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-01-15 15:43:29 +0530

    initialized the project

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+// configuring dotenv
+dotenv.config();
+
+// database
+connectDB();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "welcome the ecommerce app",
+  });
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`.bgCyan.white);
+});
